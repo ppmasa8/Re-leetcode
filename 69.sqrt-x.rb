5bfb5f384cfe894e32bcb7139cf0f6b3b84e5ce0
ppmasa8 <kiyokiyopip56@gmail.com>
2022-07-12 19:47:48 +0900

    add sqrt

---
+++ 69.sqrt-x.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=69 lang=ruby
+#
+# [69] Sqrt(x)
+#
+
+# @lc code=start
+# @param {Integer} x
+# @return {Integer}
+def my_sqrt(x)
+    return Math.sqrt(x).to_i
+end
+# @lc code=end
+
