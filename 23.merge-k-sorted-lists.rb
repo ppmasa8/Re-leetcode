d4da01657f2135eeff71cf5f02705a7a75ff996a
ppmasa8 <kiyokiyopip56@gmail.com>
2022-07-18 21:43:10 +0900

    add

---
+++ 23.merge-k-sorted-lists.rb
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=23 lang=ruby
+#
+# [23] Merge k Sorted Lists
+#
+
+# @lc code=start
+# Definition for singly-linked list.
+# class ListNode
+#     attr_accessor :val, :next
+#     def initialize(val = 0, _next = nil)
+#         @val = val
+#         @next = _next
+#     end
+# end
+# @param {ListNode[]} lists
+# @return {ListNode}
+def merge_k_lists(lists)
+    
+end
+# @lc code=end
+
--- 645.set-mismatch.rb
+++ 645.set-mismatch.rb
@@ -8,7 +8,18 @@
 # @param {Integer[]} nums
 # @return {Integer[]}
 def find_error_nums(nums)
-    
+    hash = Hash.new(0)
+    missing = 0
+    nums.each do |n|
+        hash[n] += 1
+    end
+
+    (1..nums.size).each do |i|
+        if !hash.key?(i)
+            missing = i
+        end
+    end
+    [hash.key(2), missing]
 end
 # @lc code=end
 
