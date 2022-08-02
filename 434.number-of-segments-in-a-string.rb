ff85cfb68fae6a3c7241b8dbef70f4bcb1066a02
ppmasa8 <kiyokiyopip56@gmail.com>
2022-08-02 22:44:42 +0900

    add

---
+++ 434.number-of-segments-in-a-string.rb
@@ -0,0 +1,14 @@
+#
+# @lc app=leetcode id=434 lang=ruby
+#
+# [434] Number of Segments in a String
+#
+
+# @lc code=start
+# @param {String} s
+# @return {Integer}
+def count_segments(s)
+    s.split.length
+end
+# @lc code=end
+
