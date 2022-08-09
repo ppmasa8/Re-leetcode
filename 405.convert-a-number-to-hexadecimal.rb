67c5e67ed61f3e2a1603e4a0ec97483c9e65f890
ppmasa8 <kiyokiyopip56@gmail.com>
2022-08-09 17:24:32 +0900

    add

---
+++ 405.convert-a-number-to-hexadecimal.rb
@@ -0,0 +1,26 @@
+#
+# @lc app=leetcode id=405 lang=ruby
+#
+# [405] Convert a Number to Hexadecimal
+#
+
+# @lc code=start
+# @param {Integer} num
+# @return {String}
+def to_hex(num)
+    answer = ""
+    num += (0xffffffff + 1) if num < 0
+    while num >= 0
+        rest = num % 16
+        if rest < 10
+            answer = rest.to_s + answer
+        else
+            answer = (rest-10 + ?a.ord).chr + answer
+        end
+        num = num >> 4
+        break if num.zero?
+    end
+    answer
+end
+# @lc code=end
+
